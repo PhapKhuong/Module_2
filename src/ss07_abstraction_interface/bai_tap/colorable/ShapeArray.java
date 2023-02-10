1c460c9bfc0c8ab2b2be2fb5554dd4f7bf8d3029
PhapKhuong <phapkhuong@gmail.com>
2023-02-10 14:52:11 +0700

    Add bài Colorable

---
+++ src/ss07_abstraction_interface/bai_tap/colorable/ShapeArray.java
@@ -0,0 +1,23 @@
+package ss07_abstraction_interface.bai_tap.colorable;
+
+public class ShapeArray {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle(5);
+        shapes[1] = new Rectangle(2, 3);
+        shapes[2] = new Square(10);
+
+        for (Shape shape : shapes) {
+            if (shape instanceof Circle) {
+                System.out.println("Diện tích hình tròn là: " + ((Circle) shape).getArea());
+            } else if (shape instanceof Rectangle) {
+                System.out.println("Diện tích hình chữ nhật là: " + ((Rectangle) shape).getArea());
+            } else if (shape instanceof Square) {
+                System.out.println("Diện tích hình vuông là: " + ((Square) shape).getArea());
+                ((Square) shape).howToColor();
+            } else {
+                System.out.println("No information");
+            }
+        }
+    }
+}
